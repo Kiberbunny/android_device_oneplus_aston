a96a918b4628ecbd20895234c84da03518fd3f9f
Kiberbunny <156315376+Kiberbunny@users.noreply.github.com>
2026-09-06 14:35:04 +0400

    Add files via upload

---
+++ aston/custom_aston.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021-2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from aston device
+$(call inherit-product, device/oneplus/aston/device.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+PRODUCT_NAME := custom_aston
+PRODUCT_DEVICE := aston
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2585
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1778037603666 release-keys" \
+    BuildFingerprint=OnePlus/CPH2585IN/OP5D35L1:16/TP1A.220905.001/U.R4T3.14b8271_eeda2c_f016dc:user/release-keys \
+    DeviceName=OP5D35L1 \
+    DeviceProduct=CPH2585 \
+    SystemDevice=OP5D35L1 \
+    SystemName=CPH2585
